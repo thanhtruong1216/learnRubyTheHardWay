2e5aa40b8de04f55ca9c82e7bf75a267fe51bfba
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 22:13:02 +0700

    feat: write comments above each line puts

--- ex4.rb
+++ ex4.rb
@@ -1,32 +1,43 @@
 # Exercise 4: Variables and Names
 
-# Declare variable cars with value equal is 100
+# Declare variable cars with value equal to 100
 cars = 100
 
-# Declare variable pace_in_a_car with value equal is 4.0
+# Declare variable pace_in_a_car with value equal to 4.0
 space_in_a_car = 4.0
 
-# Declare variable drivers with value equal is 30
+# Declare variable drivers with value equal to 30
 drivers = 30
 
-# Declare variable passengers with value equal is 90
+# Declare variable passengers with value equal to 90
 passengers = 90
 
 # Declare variable cars_not_driven with value equal is (cars - drivers)
 cars_not_driven = cars - drivers
 
-# Declare variable cars_driven with value equal variable drivers
+# Declare variable cars_driven with value equal to variable drivers
 cars_driven = drivers
 
-# Declare variable carpool_capacity with value equal is (cars_driven * space_in_a_car)
+# Declare variable carpool_capacity with value equal to (cars_driven * space_in_a_car)
 carpool_capacity = cars_driven * space_in_a_car
 
-# Declare variable average_passengers_per_car with value equal (passengers / cars_driven)
+# Declare variable average_passengers_per_car with value equal to (passengers / cars_driven)
 average_passengers_per_car = passengers / cars_driven
 
+# Print the string 'There are 100 cars available.'
 puts "There are #{cars} cars available."
+
+# Print the string 'There are only 30 drivers available.'
 puts "There are only #{drivers} drivers available."
+
+# Print the string 'There will be 70 empty cars today.'
 puts "There will be #{cars_not_driven} empty cars today."
+
+# Print the string We can transport 120.0 people today.
 puts "We can transport #{carpool_capacity} people today."
+
+# Print the string 'We have 90 to carpool today.'
 puts "We have #{passengers} to carpool today."
+
+# Print the string 'We need to put about 3 in each car.'
 puts "We need to put about #{average_passengers_per_car} in each car."
