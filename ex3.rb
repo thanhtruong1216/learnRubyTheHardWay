706dfdcfd7a8ef4577060db8134ede7f57a41a7f
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 22:01:58 +0700

    refactor: rewrite comments

--- ex3.rb
+++ ex3.rb
@@ -1,41 +1,41 @@
-# Print the string
-puts "I will now count my chickens:"
+# Print the string 'I will now count my chickens:'
+puts 'I will now count my chickens:'
 
-# Print the string with doing a math
+# Print 'Hens 30'
 puts "Hens #{25 + 30 / 6}"
 
-# Print the string with doing a math
+# Print 'Roosters 97'
 puts "Roosters #{100 - 25 * 3 % 4}"
 
-# Print the string
-puts "Now I will count the eggs:"
+# Print the string 'Now I will count the eggs:'
+puts 'Now I will count the eggs:'
 
-# Print the result of a math
+# Print 7
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
-# Print the string
-puts "Is it true that 3 + 2 < 5 - 7?"
+# Print the string 'Is it true that 3 + 2 < 5 - 7?'
+puts 'Is it true that 3 + 2 < 5 - 7?'
 
-# Print the result of a math
+# Print false
 puts 3 + 2 < 5 - 7
 
-# Print the string with doing a math
+# Print 'What is 3 + 2? 5'
 puts "What is 3 + 2? #{3 + 2}"
 
-# Print the string with doing a math
+# Print 'What is 5 - 7? -2'
 puts "What is 5 - 7? #{5 - 7}"
 
-# Print the string
+# Print 'Oh, that's why it's false.'
 puts "Oh, that's why it's false."
 
-# Print the string
-puts "How about some more."
+# Print the string 'How about some more.'
+puts 'How about some more.'
 
-# Print the string with doing a math
+# Print 'Is it greater? true'
 puts "Is it greater? #{5 > -2}"
 
-# Print the string with doing a math
+# Print 'Is it greater or equal? true'
 puts "Is it greater or equal? #{5 >= -2}"
 
-# Print the string with doing a math
+# Print 'Is it less or equal? false'
 puts "Is it less or equal? #{5 <= -2}"
