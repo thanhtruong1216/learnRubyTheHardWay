9bcd2a48108d09ff54185920c1129efae7fc7dea
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 16:53:46 +0700

    feat: write the variables to convert height to centimenters and weight to kilograms

--- ex5.rb
+++ ex5.rb
@@ -1,4 +1,5 @@
 # Exercise 5: More Variables and Printing
+
 name = 'Zed A. Shaw'
 age = 35 # not a lie in 2009
 height = 74 # inches
@@ -6,6 +7,14 @@ weight = 180 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+
+# convert inches to centiment
+inches_to_centimet = height * 2.54
+puts inches_to_centimet
+
+# convert pound to kilogram
+pound_to_kilogram = weight * 0.45359237
+puts pound_to_kilogram
 
 puts "Let's talk about #{name}."
 puts "He's #{height} inches tall."
