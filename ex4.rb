6349ebc08f26ebb943363cd2266b1e22d03b9c2c
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 16:42:37 +0700

    feat: printing more variables

--- ex4.rb
+++ ex4.rb
@@ -24,7 +24,6 @@ carpool_capacity = cars_driven * space_in_a_car
 # Declare variable average_passengers_per_car with value equal (passengers / cars_driven)
 average_passengers_per_car = passengers / cars_driven
 
-
 puts "There are #{cars} cars available."
 puts "There are only #{drivers} drivers available."
 puts "There will be #{cars_not_driven} empty cars today."
