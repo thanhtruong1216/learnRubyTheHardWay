5982d34f62d06723d81d5bc4ea82671fc1aab8e3
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 17:22:03 +0700

    feat: write comments to explaining the codes

---
+++ ex7.rb
@@ -0,0 +1,51 @@
+# Print q string
+puts 'Mary had a little lamb.'
+
+# Print a string including a word 'snow'
+puts "Its fleece was white as #{'snow'}."
+
+# Print q string
+puts 'And everywhere that Mary went.'
+
+# Print the '.' 10 times
+puts '.' * 10
+
+# Declare variable end1 equal to 'C'
+end1 = 'C'
+
+# Declare variable end1 equal to 'h'
+end2 = 'h'
+
+# Declare variable end1 equal to 'e'
+end3 = 'e'
+
+# Declare variable end1 equal to 'e'
+end4 = 'e'
+
+# Declare variable end1 equal to 's'
+end5 = 's'
+
+# Declare variable end1 equal to 'e'
+end6 = 'e'
+
+# Declare variable end1 equal to 'B'
+end7 = 'B'
+
+# Declare variable end1 equal to 'u'
+end8 = 'u'
+
+# Declare variable end1 equal to 'r'
+end9 = 'r'
+
+# Declare variable end1 equal to 'g'
+end10 = 'g'
+
+# Declare variable end1 equal to 'e'
+end11 = 'e'
+
+# Declare variable end1 equal to 'r'
+end12 = 'r'
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
