c6d6983292fe9da9283c5ac6160c1e5cf927c150
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 22:19:21 +0700

    feat: write comments to explain the codes

--- ex5.rb
+++ ex5.rb
@@ -1,29 +1,43 @@
 # Exercise 5: More Variables and Printing
 
+# Change all the variables so there is no my_ in front of each one
 name = 'Zed A. Shaw'
-age = 35 # not a lie in 2009
-height = 74 # inches
-weight = 180 # lbs
+age = 35
+height = 74 # Heiht in inches
+weight = 180 # Weight in kilograms
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
-
 # convert inches to centimenters
-inches_to_centimet = height * 2.54
-puts inches_to_centimet
+inches_to_centimets = height * 2.54
+
+# Print 187.96
+puts inches_to_centimets
 
 # convert pounds to kilograms
+pound_to_kilograms = weight * 0.45359237
 
-pound_to_kilogram = weight * 0.45359237
-puts pound_to_kilogram
+# Print 81.6466266
+puts pound_to_kilograms
 
+# Print the string 'Let's talk about Zed A. Shaw.'
 puts "Let's talk about #{name}."
+
+# Print the string 'He's 74 inches tall.'
 puts "He's #{height} inches tall."
+
+# Print the string 'He's 180 pounds heavy.'
 puts "He's #{weight} pounds heavy."
+
+# Print the string 'Actually that's not too heavy.'
 puts "Actually that's not too heavy."
+
+# Print the string 'He's got Blue eyes and Brown hair.'
 puts "He's got #{eyes} eyes and #{hair} hair."
+
+# Print the string 'His teeth are usually White depending on the coffee.'
 puts "His teeth are usually #{teeth} depending on the coffee."
 
-# this line is tricky, try to get it exactly right
+# Print the string 'If I add 35, 74, and 180 I get 289.'
 puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
