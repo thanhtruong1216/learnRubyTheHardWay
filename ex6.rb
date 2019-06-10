b09a38cb49bc24e57f8cdbc5a5ecb2918e9fadef
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 22:26:28 +0700

    refactor: rewrite the comments

--- ex6.rb
+++ ex6.rb
@@ -4,25 +4,25 @@ types_of_people = 10
 # Declare variable x is a string including variable types_of_people
 x = "There are #{types_of_people} types of people."
 
-# Declare variable binary is a string
+# Declare variable binary equal to a string 'binary'
 binary = 'binary'
 
-# Declare variable do_not is a string
+# Declare variable do_not equal to a string "don't"
 do_not = "don't"
 
 # Declare variable y js a string including two variables: binary and do_not
 y = "Those who know #{binary} and those who #{do_not}."
 
-# Print variable x
+# Print 'There are 10 types of people.'
 puts x
 
-# Print variable y
+# Print 'Those who know binary and those who don't.'
 puts y
 
-# Print string including variable x
+# Print string 'I said: There are 10 types of people..'
 puts "I said: #{x}."
 
-# Print string including variable y
+# Print string 'I also said: 'Those who know binary and those who don't.'
 puts "I also said: '#{y}'."
 
 # Declare variable hilarious equal to false
@@ -31,14 +31,14 @@ hilarious = false
 # Declare variable joke_evaluation equal to a string including variable hilarious
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
-# Print variable joke_evaluation
+# Print 'Isn't that joke so funny?! false'
 puts joke_evaluation
 
-# Declare variable w equal to a string
+# Declare variable w equal to a string ''This is the left side of...'
 w = 'This is the left side of...'
 
-# Declare variable e equal to a string
+# Declare variable e equal to a string 'a string with a right side.'
 e = 'a string with a right side.'
 
-# Print the result of concat two string w and e
+# Print the string 'This is the left side of...a string with a right side.'
 puts w + e
