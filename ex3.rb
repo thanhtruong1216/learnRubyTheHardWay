467f74025e3abe11122e942ef9eaa6f2fb7d6063
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 16:27:32 +0700

    feat: write comment to explaining the codes

---
+++ ex3.rb
@@ -0,0 +1,41 @@
+# Print the string
+puts "I will now count my chickens:"
+
+# Print the string with doing a math
+puts "Hens #{25 + 30 / 6}"
+
+# Print the string with doing a math
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# Print the string
+puts "Now I will count the eggs:"
+
+# Print the result of a math
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Print the string
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Print the result of a math
+puts 3 + 2 < 5 - 7
+
+# Print the string with doing a math
+puts "What is 3 + 2? #{3 + 2}"
+
+# Print the string with doing a math
+puts "What is 5 - 7? #{5 - 7}"
+
+# Print the string
+puts "Oh, that's why it's false."
+
+# Print the string
+puts "How about some more."
+
+# Print the string with doing a math
+puts "Is it greater? #{5 > -2}"
+
+# Print the string with doing a math
+puts "Is it greater or equal? #{5 >= -2}"
+
+# Print the string with doing a math
+puts "Is it less or equal? #{5 <= -2}"
