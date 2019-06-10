9a64e6cd584c602398b0d49de53af85a4abe3041
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 17:06:01 +0700

    feat: write the comments to explaining the codes

---
+++ ex6.rb
@@ -0,0 +1,44 @@
+# Declare variable types_of_people to hold the number of people types
+types_of_people = 10
+
+# Declare variable x is a string including variable types_of_people
+x = "There are #{types_of_people} types of people."
+
+# Declare variable binary is a string
+binary = "binary"
+
+# Declare variable do_not is a string
+do_not = "don't"
+
+# Declare variable y js a string including two variables: binary and do_not
+y = "Those who know #{binary} and those who #{do_not}."
+
+# Print variable x
+puts x
+
+# Print variable y
+puts y
+
+# Print string including variable x
+puts "I said: #{x}."
+
+# Print string including variable y
+puts "I also said: '#{y}'."
+
+# Declare variable hilarious equal to false
+hilarious = false
+
+# Declare variable joke_evaluation equal to a string including variable hilarious
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# Print variable joke_evaluation
+puts joke_evaluation
+
+# Declare variable w equal to a string
+w = "This is the left side of..."
+
+# Declare variable e equal to a string
+e = "a string with a right side."
+
+# Print the result of concat two string w and e
+puts w + e
