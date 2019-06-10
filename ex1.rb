1d743b90165dff7670a97463c99499c7abd45b81
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 12:20:30 +0700

    feat: example 1 - A good first program

--- ex1.rb
+++ ex1.rb
@@ -1,4 +1,16 @@
 # Exercise 1: A Good First Program
+
 # To do: Make script print another line.
+puts 'Hello world'
+puts 'Beautiful day'
+puts 'This is fun'
+
 # To do: Make script print only one of the lines.
-# To do: Put a # character at the beginning of a line. What did it do? Try to find out what this character does.
+puts 'Hello world'
+# puts 'Beautiful day'
+# puts 'This is fun'
+
+# To do: Put a # character at the beginning of a line.
+# Puts 'Hello world'
+# puts 'Beautiful day'
+# puts 'This is fun'
