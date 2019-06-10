26e708d01e294c846cceb8184f656810c7e09e65
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 21:44:39 +0700

    feat: write comments to explain the codes

--- ex8.rb
+++ ex8.rb
@@ -1,11 +1,24 @@
+# Declare the variable formatter
 formatter = '%{first} %{second} %{third} %{fourth}'
 
-# Print the string apply the same format to multiple values
+# Uses variable formatter to print 1, 2, 3, 4
 puts formatter % { first: 1, second: 2, third: 3, fourth: 4 }
+
+# Uses variable formatter to print one, two, three, four
 puts formatter % { first: 'one', second: 'two', third: 'three', fourth: 'four' }
+
+# Uses variable formatter to print true, false, true, false
 puts formatter % { first: true, second: false, third: true, fourth: false }
+
+# Uses variable formatter to print true, false, true, false
 puts formatter % { first: formatter, second: formatter, third: formatter, fourth: formatter }
 
+=begin
+  Uses variable formatter to print the strings:
+  'I had this thing.',
+  'That you could type up right.',
+  "But it didn't sing.", 'So I said goodnight.'
+=end
 puts formatter % {
   first: 'I had this thing.',
   second: 'That you could type up right.',
