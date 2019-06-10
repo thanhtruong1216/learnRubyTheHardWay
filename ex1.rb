85b6ff0fb40b6613531829a871e854312767a7e2
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-10 12:10:17 +0700

    chore: config rubocop

---
+++ ex1.rb
@@ -0,0 +1,4 @@
+# Exercise 1: A Good First Program
+# To do: Make script print another line.
+# To do: Make script print only one of the lines.
+# To do: Put a # character at the beginning of a line. What did it do? Try to find out what this character does.
