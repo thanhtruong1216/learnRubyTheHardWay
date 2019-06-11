4ba03837311afff796ece0a252d189ff1454ecbf
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-11 10:22:46 +0700

    feat: use argument variable

---
+++ ex13.rb
@@ -0,0 +1,9 @@
+# Exercise 13: Parameters, Unpacking, Variables
+
+# Define argument variable
+first, second, third = ARGV
+
+# Using argument variable and print out
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
