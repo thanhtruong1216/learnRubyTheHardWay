eaa01b319cd70f50c4ba1cec7c3fb7ff3673951b
Thanh Truong <thanhtruong1216@gmail.com>
2019-06-11 09:42:16 +0700

    feat: get input from user and change it

---
+++ ex11.rb
@@ -0,0 +1,27 @@
+# Take name from a user
+print 'What your name?'
+age = gets.chomp
+
+# Change the input to upcase and print out
+puts age.upcase
+
+# Take hometown from user
+print 'Where are you from?'
+hometown = gets.chomp
+
+# Find the length of homtown input
+puts hometown.length
+
+# Take the home address from user
+print 'Where do you live?'
+home = gets.chomp
+
+# Change the input to downcase and print out
+puts home.downcase
+
+# Get the age from the user
+print 'How old are you?'
+age = gets.chomp
+
+# Reverse the age input and print out
+puts age.reverse
